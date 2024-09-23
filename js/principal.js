7f6fed5485a863f827c849a852d213f4d2b48c8a
AlfredoNeves13 <163014698+AlfredoNeves13@users.noreply.github.com>
2024-09-23 12:13:32 -0300

    js

--- js/principal.js
+++ js/principal.js
@@ -11,8 +11,8 @@ var pacientes =document.querySelectorAll(".paciente");
 
 for(let contador = 0;contador <pacientes.length; contador++){
     var paciente = pacientes[contador]
-    var peso = paciente.querySelector ("info-peso").textContent;
-    var altura = paciente.querySelector ("info-altura").textContent;
+    var peso = paciente.querySelector("info-peso").textContent;
+    var altura = paciente.querySelector("info-altura").textContent;
     var imc = peso/(altura * altura);
-    paciente.querySelector (".info-imc").textContent = imc; 
+    paciente.querySelector(".info-imc").textContent = imc; 
 }
