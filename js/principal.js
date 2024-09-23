8d2107a832ac6e905f50992cbe459fbd713f7031
AlfredoNeves13 <163014698+AlfredoNeves13@users.noreply.github.com>
2024-09-23 12:11:49 -0300

    js

--- js/principal.js
+++ js/principal.js
@@ -3,7 +3,7 @@ titulo.textContent = "Aparecida Nutricionista"
 
 //var paulo = document.querySelector("#paciente1");
 //var peso = document.querySelector(".info-peso").textContent;
-//var altura = document. querySelector(".info-altura").textContent;
+//var altura = document.querySelector(".info-altura").textContent;
 //var imc = peso / (altura * altura);
 //paulo.querySelect (".info-imc").textContent = imc;
 
