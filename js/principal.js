93146648d5ec19c7b62f20a4aff07d468c0f8491
AlfredoNeves13 <163014698+AlfredoNeves13@users.noreply.github.com>
2024-09-24 08:17:16 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista"
+titulo.textContent = "Aparecida Nutricionista";
 
 //var paulo = document.querySelector("#paciente1");
 //var peso = document.querySelector(".info-peso").textContent;
@@ -9,7 +9,7 @@ titulo.textContent = "Aparecida Nutricionista"
 
 var pacientes =document.querySelectorAll(".paciente");
 
-for(let contador = 0;contador <pacientes.length; contador++){
+for(let contador = 0;contador < pacientes.length; contador++){
     var paciente = pacientes[contador]
     var peso = paciente.querySelector("info-peso").textContent;
     var altura = paciente.querySelector("info-altura").textContent;
